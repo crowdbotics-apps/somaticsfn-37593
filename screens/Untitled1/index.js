906ee8ffba35bb4cde588ca58af1c88d7f893044
Somatics Fitness <Inquiries@somaticsfn.com>
2022-11-15 20:45:59 -0500

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.YHjnBxyk}>
+      <View style={_styles.PtDnvgPG}></View></View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  YHjnBxyk: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%",
+    borderRadius: 0
+  },
+  PtDnvgPG: {
+    left: -88,
+    top: -40,
+    position: "absolute",
+    height: 779,
+    width: 531,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
---
+++ screens/Untitled2/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled2 = () => {
+  return <View style={_styles.iDmxcKEL}>
+      <View style={_styles.wGpZAFsq}></View></View>;
+};
+
+export default Untitled2;
+
+const _styles = StyleSheet.create({
+  iDmxcKEL: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  wGpZAFsq: {
+    left: -120,
+    top: -40,
+    position: "absolute",
+    height: 914,
+    width: 705,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
