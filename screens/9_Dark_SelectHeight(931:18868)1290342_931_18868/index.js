bdb70ebb8bb57bf5234ece47a0a61ee260480425
Crowdbotics <team@crowdbotics.com>
2022-11-16 02:18:05 +0000

    Update components

---
+++ screens/9_Dark_SelectHeight(931:18868)1290342_931_18868/index.js
@@ -0,0 +1,544 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_931_18869}>
+        <View style={styles.View_931_18870}>
+          <View style={styles.View_931_18872}>
+            <View style={styles.View_931_18873}>
+              <Text style={styles.Text_931_18873}>How Tall Are You?</Text>
+            </View>
+            <View style={styles.View_931_18874}>
+              <Text style={styles.Text_931_18874}>
+                Height in inches. Don&#39;t worry, you can always change it
+                later!
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.View_931_18875}>
+          <View style={styles.View_931_18876}>
+            <Text style={styles.Text_931_18876}>5’5</Text>
+          </View>
+          <View style={styles.View_931_18877}>
+            <Text style={styles.Text_931_18877}>5’6</Text>
+          </View>
+          <View style={styles.View_931_18878}>
+            <Text style={styles.Text_931_18878}>5’7</Text>
+          </View>
+          <View style={styles.View_931_18879} />
+          <View style={styles.View_931_18880}>
+            <Text style={styles.Text_931_18880}>5’8</Text>
+          </View>
+          <View style={styles.View_931_18881} />
+          <View style={styles.View_931_18882}>
+            <Text style={styles.Text_931_18882}>5’9</Text>
+          </View>
+          <View style={styles.View_931_18883}>
+            <Text style={styles.Text_931_18883}>5’10</Text>
+          </View>
+          <View style={styles.View_931_18884}>
+            <Text style={styles.Text_931_18884}>5’11</Text>
+          </View>
+        </View>
+        <View style={styles.View_931_18885}>
+          <View style={styles.View_931_18886}>
+            <View style={styles.View_I931_18886_442_1601}>
+              <Text style={styles.Text_I931_18886_442_1601}>Back</Text>
+            </View>
+          </View>
+          <View style={styles.View_931_18887}>
+            <View style={styles.View_I931_18887_433_784}>
+              <Text style={styles.Text_I931_18887_433_784}>Continue</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.View_931_18888}>
+        <View style={styles.View_I931_18888_119_97}>
+          <Text style={styles.Text_I931_18888_119_97}>9:41</Text>
+        </View>
+        <View style={styles.View_I931_18888_119_98}>
+          <ImageBackground
+            source={{
+              uri:
+                "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/a732da06-7bd3-49d1-910d-2ee27206c0db"
+            }}
+            style={styles.ImageBackground_I931_18888_119_99}
+          />
+        </View>
+        <View style={styles.View_I931_18888_119_104}>
+          <ImageBackground
+            source={{
+              uri:
+                "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/8dcf82cc-9bd4-43a5-a333-656ce51d19b5"
+            }}
+            style={styles.ImageBackground_I931_18888_119_105}
+          />
+        </View>
+        <View style={styles.View_I931_18888_119_109}>
+          <ImageBackground
+            source={{
+              uri:
+                "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/3487b721-31f5-4509-a4bc-ce1b5d39d072"
+            }}
+            style={styles.ImageBackground_I931_18888_119_110}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(24, 26, 32, 1)" },
+  View_2: { height: hp("127%") },
+  View_931_18869: {
+    flexGrow: 1,
+    width: wp("100%"),
+    height: hp("120%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("6%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_931_18870: {
+    width: wp("89%"),
+    minWidth: wp("89%"),
+    height: hp("14%"),
+    minHeight: hp("14%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("6%"),
+    top: hp("7%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_931_18872: {
+    width: wp("89%"),
+    minWidth: wp("89%"),
+    height: hp("14%"),
+    minHeight: hp("14%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_931_18873: {
+    width: wp("89%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    justifyContent: "center"
+  },
+  Text_931_18873: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 26,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_931_18874: {
+    width: wp("89%"),
+    minWidth: wp("89%"),
+    minHeight: hp("7%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("7%"),
+    justifyContent: "center"
+  },
+  Text_931_18874: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 14,
+    fontWeight: "400",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0.20000000298023224,
+    textTransform: "none"
+  },
+  View_931_18875: {
+    width: wp("22%"),
+    minWidth: wp("22%"),
+    height: hp("68%"),
+    minHeight: hp("68%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("39%"),
+    top: hp("29%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_931_18876: {
+    width: wp("8%"),
+    minWidth: wp("8%"),
+    minHeight: hp("4%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("7%"),
+    top: hp("0%"),
+    justifyContent: "center"
+  },
+  Text_931_18876: {
+    color: "rgba(224, 224, 224, 1)",
+    fontSize: 19,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_931_18877: {
+    width: wp("10%"),
+    minWidth: wp("10%"),
+    minHeight: hp("5%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("6%"),
+    top: hp("7%"),
+    justifyContent: "center"
+  },
+  Text_931_18877: {
+    color: "rgba(224, 224, 224, 1)",
+    fontSize: 26,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_931_18878: {
+    width: wp("16%"),
+    minWidth: wp("16%"),
+    minHeight: hp("8%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("3%"),
+    top: hp("14%"),
+    justifyContent: "center"
+  },
+  Text_931_18878: {
+    color: "rgba(245, 245, 245, 1)",
+    fontSize: 40,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_931_18879: {
+    width: wp("22%"),
+    minWidth: wp("22%"),
+    height: hp("1%"),
+    minHeight: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("25%")
+  },
+  View_931_18880: {
+    width: wp("22%"),
+    minWidth: wp("22%"),
+    minHeight: hp("11%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("28%"),
+    justifyContent: "center"
+  },
+  Text_931_18880: {
+    fontSize: 56,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_931_18881: {
+    width: wp("22%"),
+    minWidth: wp("22%"),
+    height: hp("1%"),
+    minHeight: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("43%")
+  },
+  View_931_18882: {
+    width: wp("16%"),
+    minWidth: wp("16%"),
+    minHeight: hp("8%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("3%"),
+    top: hp("46%"),
+    justifyContent: "center"
+  },
+  Text_931_18882: {
+    color: "rgba(245, 245, 245, 1)",
+    fontSize: 40,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_931_18883: {
+    width: wp("13%"),
+    minWidth: wp("13%"),
+    minHeight: hp("5%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("4%"),
+    top: hp("57%"),
+    justifyContent: "center"
+  },
+  Text_931_18883: {
+    color: "rgba(224, 224, 224, 1)",
+    fontSize: 26,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_931_18884: {
+    width: wp("8%"),
+    minWidth: wp("8%"),
+    minHeight: hp("4%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("7%"),
+    top: hp("64%"),
+    justifyContent: "center"
+  },
+  Text_931_18884: {
+    color: "rgba(224, 224, 224, 1)",
+    fontSize: 19,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_931_18885: {
+    width: wp("89%"),
+    minWidth: wp("89%"),
+    height: hp("8%"),
+    minHeight: hp("8%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("6%"),
+    top: hp("106%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_931_18886: {
+    width: wp("43%"),
+    minWidth: wp("43%"),
+    height: hp("8%"),
+    minHeight: hp("8%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(53, 56, 63, 1)"
+  },
+  View_I931_18886_442_1601: {
+    flexGrow: 1,
+    width: wp("36%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("4%"),
+    top: hp("2%"),
+    justifyContent: "center"
+  },
+  Text_I931_18886_442_1601: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 13,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0.20000000298023224,
+    textTransform: "none"
+  },
+  View_931_18887: {
+    width: wp("43%"),
+    minWidth: wp("43%"),
+    height: hp("8%"),
+    minHeight: hp("8%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("46%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I931_18887_433_784: {
+    flexGrow: 1,
+    width: wp("36%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("4%"),
+    top: hp("2%"),
+    justifyContent: "center"
+  },
+  Text_I931_18887_433_784: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 13,
+    fontWeight: "700",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0.20000000298023224,
+    textTransform: "none"
+  },
+  View_931_18888: {
+    width: wp("100%"),
+    minWidth: wp("100%"),
+    height: hp("6%"),
+    minHeight: hp("6%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I931_18888_119_97: {
+    flexGrow: 1,
+    width: wp("6%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("5%"),
+    top: hp("2%"),
+    justifyContent: "center"
+  },
+  Text_I931_18888_119_97: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 13,
+    fontWeight: "600",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0.20000000298023224,
+    textTransform: "none"
+  },
+  View_I931_18888_119_98: {
+    flexGrow: 1,
+    width: wp("4%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("78%"),
+    top: hp("2%")
+  },
+  ImageBackground_I931_18888_119_99: {
+    width: wp("4%"),
+    minWidth: wp("4%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%")
+  },
+  View_I931_18888_119_104: {
+    flexGrow: 1,
+    width: wp("4%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("83%"),
+    top: hp("2%")
+  },
+  ImageBackground_I931_18888_119_105: {
+    width: wp("4%"),
+    minWidth: wp("4%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%")
+  },
+  View_I931_18888_119_109: {
+    flexGrow: 1,
+    width: wp("6%"),
+    height: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("88%"),
+    top: hp("2%")
+  },
+  ImageBackground_I931_18888_119_110: {
+    width: wp("6%"),
+    minWidth: wp("6%"),
+    height: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%")
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
